5d3f397462b5806b9518a2a2959fb4d2ea3be600
jc <emclab2011@gmail.com>
2014-04-27 16:42:41 -0500

    JC: added a few fields to post model for future finer access control. spec passes.

--- app/models/post_boardx/post.rb
+++ app/models/post_boardx/post.rb
@@ -1,9 +1,12 @@
 module PostBoardx
   class Post < ActiveRecord::Base
-    attr_accessor :submitted_by_name, :category_name, :last_updated_by_name
+    attr_accessor :submitted_by_name, :category_name, :last_updated_by_name, :post_zone_name, :post_role_name, :post_group_name, :post_task_name
     attr_accessible :category_id, :content, :last_updated_by_id, :subject, :submitted_by_id, :expire_date, :start_date,
+                    :post_zone_id, :post_role_id, :post_group_id, :post_task_id,
                     :as => :role_new                  
     attr_accessible :category_id, :content, :subject, :submitted_by_name, :expire_date, :start_date, :last_updated_by_name,
+                    :post_zone_id, :post_role_id, :post_group_id, :post_task_id,
+                    :post_zone_name, :post_role_name, :post_group_name, :post_task_name,
                     :as => :role_update   
                     
     belongs_to :last_updated_by, :class_name => 'Authentify::User'
--- db/migrate/20140423030839_create_post_boardx_posts.rb
+++ db/migrate/20140423030839_create_post_boardx_posts.rb
@@ -8,6 +8,10 @@ class CreatePostBoardxPosts < ActiveRecord::Migration
       t.integer :category_id
       t.date :start_date
       t.date :expire_date
+      t.integer :post_zone_id
+      t.integer :post_role_id
+      t.integer :post_group_id  #group here is the sys_user_group in authentify
+      t.integer :post_task_id
 
       t.timestamps
     end
@@ -15,5 +19,6 @@ class CreatePostBoardxPosts < ActiveRecord::Migration
     add_index :post_boardx_posts, :submitted_by_id
     add_index :post_boardx_posts, :subject
     add_index :post_boardx_posts, :category_id
+    add_index :post_boardx_posts, :post_zone_id
   end
 end
